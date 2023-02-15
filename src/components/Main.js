c798978de4e8e2b7d57d3b264556eedc9244c312
Salvarez0212 <alvarezsebastian31@gmail.com>
2023-02-15 18:13:12 -0500

    Simbolos de las unidades añadidos

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,70 +1,65 @@
-import React, {useState} from 'react'
-import './styles.css'
-
-
-
+import React, { useState } from "react";
+import "./styles.css";
 
 export default function Main() {
-
   const [degree, setDegree] = useState(0);
-  const [unit, setUnit] = useState("celsius")
-  const [result, setResult] = useState()
-  const handleChange=(e) => {setDegree(parseInt(e.target.value))}
+  const [unit, setUnit] = useState("celsius");
+  const [result, setResult] = useState();
+  const handleChange = (e) => {
+    setDegree(parseInt(e.target.value));
+  };
 
-
-  const converter = () =>{
-   
-    if (unit ==='fahrenheit'){
-      return setResult((degree-32)*5/9)
+  const converter = () => {
+    if (unit === "fahrenheit") {
+      return setResult(((degree - 32) * 5) / 9 + "°C");
+    } else {
+      return setResult((degree * 9) / 5 + 32 + "°F");
     }
-    else{
-      return setResult((degree*9/5)+32)
-    }
-  }
+  };
 
   //El estado siempre representa el valor del Input (componente controlado)
 
   return (
-    <section className='degree-card'>
-      <div className='inputs'>
-          <div className='degrees'>
-         <label>
-           Degrees
-          <input 
-            className='number' 
-            type="number" 
-            name="" 
-            value={degree} 
-            onChange={handleChange}
-          />
+    <section className="degree-card">
+      <div className="inputs">
+        <div className="degrees">
+          <label>
+            Degrees
+            <input
+              className="number"
+              type="number"
+              name=""
+              value={degree}
+              onChange={handleChange}
+            />
           </label>
-          </div>
-
-          <div className='type'>
-         <label>
-           Type
-          <select value={unit} className='degree-options' onChange={(e) => {setUnit(e.target.value)}}> 
-            <option value="fahrenheit">Fahrenheit</option>
-            <option value="celsius">Celsius</option>
-          </select>
-             </label>
-          </div>
-
-         <button className='convert-button' onClick={converter} >
-          Convert
-          </button>
         </div>
 
-      <div className='results'>
-      <label>Result:</label>
-      <h2>{result}</h2>
-      
+        <div className="type">
+          <label>
+            Type
+            <select
+              value={unit}
+              className="degree-options"
+              onChange={(e) => {
+                setUnit(e.target.value);
+              }}
+            >
+              <option value="fahrenheit">Fahrenheit</option>
+              <option value="celsius">Celsius</option>
+            </select>
+          </label>
+        </div>
+
+        <button className="convert-button" onClick={converter}>
+          Convert
+        </button>
       </div>
 
+      <div className="results">
+        <label>Result:</label>
+        <h2>{result}</h2>
+      </div>
     </section>
-  )
+  );
 }
-
-
-
-
